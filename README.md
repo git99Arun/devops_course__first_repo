0e69049d258a0493eec14ca26c0e5377b8dea3b9
Aruna T <arunathnl@gmail.com>
2026-02-11 12:10:49 +0530

    feat:first changr in readme,md file and added java file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # devops_course__first_repo
 deveops first repository
+#This my first repo 
---
+++ first.java
@@ -0,0 +1,6 @@
+class Arun{
+    public static void main(String []args)
+    {
+        System.out.println("hi");
+    }
+}
